db65dade6439b5f601cd63c6953f3311682ea93a
Nils Asejevs <nasejevs@evolutiongaming.com>
2019-03-15 20:52:28 +0200

    Fix correct JSON UID sending

--- src/scanner.js
+++ src/scanner.js
@@ -15,35 +15,38 @@ nfc.on('reader', async reader => {
 	// Device attached
 	console.log(`${reader.reader.name}  device attached`);
  
-    reader.on('card', card => {
+		reader.on('card', card => {
 		// Card has been read
 		console.log(`${reader.reader.name}  card detected`, card);
 
-		const uid = reader.reader.name.uid;
+		const uid = reader.card.uid;
 
-		axios.post(serverURL + "cardScanned").catch((e) => {
-			console.log("axios error: ", e);
-		}, requestConfig);
-    });
+		axios.post(serverURL + "cardScanned", {
+			uid,
+		}, requestConfig)
+			.catch((e) => {
+				console.log("axios error: ", e);
+			})
+		});
  
-    reader.on('card.off', card => {
+		reader.on('card.off', card => {
 		// Card has been removed
-        console.log(`${reader.reader.name}  card removed`, card);
-    });
+				console.log(`${reader.reader.name}  card removed`, card);
+		});
  
-    reader.on('error', err => {
+		reader.on('error', err => {
 		// Error
-        console.log(`${reader.reader.name}  an error occurred`, err);
-    });
+				console.log(`${reader.reader.name}  an error occurred`, err);
+		});
  
-    reader.on('end', () => {
+		reader.on('end', () => {
 		// Device removed
-        console.log(`${reader.reader.name}  device removed`);
-    });
+				console.log(`${reader.reader.name}  device removed`);
+		});
  
 });
  
 nfc.on('error', err => {
 	// Error
-    console.log('an error occurred', err);
+		console.log('an error occurred', err);
 });
