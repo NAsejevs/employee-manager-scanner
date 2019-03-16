bca554ed7c1978f8e18ccd5143f15b08e43753b2
Nils Asejevs <nasejevs@evolutiongaming.com>
2019-03-16 21:43:00 +0200

    fwfwefe2

--- src/scanner.js
+++ src/scanner.js
@@ -15,49 +15,48 @@ nfc.on('reader', reader => {
 	reader.aid = "F222222222";
 
 	reader.connect(CONNECT_MODE_DIRECT).then(() => {
-		reader.setBuzzerOutput(false).then(() => {
-			reader.disconnect();
-		});
+		reader.setBuzzerOutput(false);
 	});
 
 	// Device attached
 	console.log(`${reader.reader.name}  device attached`);
  
 		reader.on('card', card => {
-		// Card has been read
-		console.log(`${reader.reader.name}  card detected`, card);
+			// Card has been read
+			console.log(`${reader.reader.name}  card detected`, card);
 
-		const uid = reader.card.uid;
+			const uid = reader.card.uid;
 
-		axios.post(serverURL + "cardScanned", {
-			uid,
-		}, requestConfig)
-			.then((res) => {
-				console.log("success!: ", res);
-			})
-			.catch((e) => {
+			axios.post(serverURL + "cardScanned", {
+				uid,
+			}, requestConfig).then((res) => {
+				console.log("success!: ", res.data);
+				reader.setBuzzerOutput(true);
+			}).catch((e) => {
 				console.log("axios error: ", e);
-			})
+			});
 		});
  
 		reader.on('card.off', card => {
-		// Card has been removed
-				console.log(`${reader.reader.name}  card removed`, card);
+			// Card has been removed
+			console.log(`${reader.reader.name}  card removed`, card);
+			reader.setBuzzerOutput(true);
 		});
  
 		reader.on('error', err => {
-		// Error
-				console.log(`${reader.reader.name}  an error occurred`, err);
+			// Error
+			console.log(`${reader.reader.name}  an error occurred`, err);
 		});
  
 		reader.on('end', () => {
-		// Device removed
-				console.log(`${reader.reader.name}  device removed`);
+			// Device removed
+			console.log(`${reader.reader.name}  device removed`);
+			reader.disconnect();
 		});
  
 });
  
 nfc.on('error', err => {
 	// Error
-		console.log('an error occurred', err);
+	console.log('an error occurred', err);
 });
