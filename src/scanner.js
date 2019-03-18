446513d5c1063f588b73ee3c81857880a48d99c8
Nils Asejevs <nasejevs@evolutiongaming.com>
2019-03-18 02:20:28 +0200

    random

--- src/scanner.js
+++ src/scanner.js
@@ -80,13 +80,13 @@ nfc.on('reader', async reader => {
 
 	//console.log("OLD READER: ", reader);
 
-	reader.on('detection', async () => {
+	reader.on('detection', () => {
 		console.log("detected");
 		//console.log("NEW READER: ", reader);
 		//await reader.led(0b01011001, [0x05, 0x00, 0x01, 0x00]);
 	});
 
-	reader.on('card', async card => {
+	reader.on('card', card => {
 		//console.log("NEW READER: ", reader);
 		// handle_Iso_14443_3_Tag
 		 console.log("card event!");
