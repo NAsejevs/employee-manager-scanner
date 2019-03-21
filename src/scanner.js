7b39569e7129447b8ba1455e51c24bdeb3924872
Nils Asejevs <nasejevs@evolutiongaming.com>
2019-03-21 20:42:05 +0200

    random

--- src/scanner.js
+++ src/scanner.js
@@ -40,7 +40,7 @@ const requestConfig = {
 // - 02: The buzzer will turn on during the T2 Duration
 // - 03: The buzzer will turn on during the T1 and T2 DuratioN
 
-const errorLEDBits = 0b00100011;
+const errorLEDBits = 0b00110011;
 
 nfc.on("reader", async reader => {
 	reader.aid = "F222222222";
